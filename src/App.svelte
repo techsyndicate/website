6bad762426ae796eb7a5c817e5a59c2064605ae3
unknown <ynarula678@gmail.com>
2022-05-03 21:57:08 +0530

    redesign about section

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
     import Nav from './components/Nav.svelte';
     import LandingPage from './pages/LandingPage.svelte';
+    import AboutPage from './pages/AboutPage.svelte';
     import MemberPage from './pages/MemberPage.svelte';
     import EventPage from './pages/EventPage.svelte';
     import LearnPage from './pages/LearnPage.svelte';
 </script>
 <Nav />
-<LearnPage />
+<AboutPage />
