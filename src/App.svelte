3fdbde809de0549a0edc75643510d26e43ec22c8
unknown <ynarula678@gmail.com>
2022-05-03 13:03:12 +0530

    redesign event section

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import Nav from './components/Nav.svelte';
     import LandingPage from './pages/LandingPage.svelte';
     import MemberPage from './pages/MemberPage.svelte';
+    import EventPage from './pages/EventPage.svelte';
 </script>
 <Nav />
-<MemberPage />
+<EventPage />
---
+++ src/pages/EventPage.svelte
@@ -0,0 +1,140 @@
+<div class="event-div">
+    <h1>
+        Events
+    </h1>
+    <div class="circle-1">
+    </div>
+    <div class="circle-2"></div>
+    <div class="line-overlap"></div>
+    <div class="circle-3"></div>
+    <div class="circle-4"></div>
+    <div class="circle-5"></div>
+    <div class="circle-6"></div>
+    <div class="line">
+    </div>
+    <div class="event-blob-1">
+        <p class="event-name">
+            Training Grounds
+        </p>
+        <p class="event-duration">
+            Mar 12 - Apr 25
+        </p>
+    </div>
+    <div class="event-blob-2">
+        <p class="event-name">
+            Summer Horizon
+        </p>
+        <p class="event-duration">
+            May 20 - May 25
+        </p>
+    </div>
+    <div class="event-blob-3">
+        <p class="event-name">
+            Intech
+        </p>
+        <p class="event-duration">
+            Jun 4 - Jun 20
+        </p>
+    </div>
+    <div class="event-blob-4">
+        <p class="event-name">
+            Robotronics
+        </p>
+        <p class="event-duration">
+            Nov 2 - Nov 12
+        </p>
+    </div>
+</div>
+<style>
+    .event-div{
+        width: 100%;
+    }
+    .event-div h1{
+        font-size: 4.5vw;
+        margin: 5vw 5vw;
+        letter-spacing: 1px;
+    }
+    .line{
+        width:75%;
+        height: .75vw;
+        border-radius: 10vw;
+        background: #16e16e;
+        display: block;
+        margin: 15vw auto auto;
+    }
+    .line-overlap{
+        width: 15%;
+        height: 0.75vw;
+        border-radius: 10vw;
+        background: #4e4e4e;
+        position: absolute;
+        margin-top: 10vw;
+        margin-left: 12.5vw;
+    }
+    .circle-1,
+    .circle-2,
+    .circle-3,
+    .circle-4,
+    .circle-5,.circle-6{
+        background: #16e16e;
+        height: 1.5vw;
+        width: 1.5vw;
+        position: absolute;
+        border-radius:100%;
+        margin-top: 9.55vw;
+    }
+    .circle-1 {
+        margin-left: 11.5vw;
+        background: #4e4e4e;
+    }
+    .circle-2{
+        margin-left: 85%;
+    }
+    .circle-3{
+        margin-left: 26%;
+    }
+    .circle-4{
+        margin-left: 40.5%;
+    }
+    .circle-5{
+        margin-left: 55%;
+    }
+    .circle-6{
+        margin-left: 69.5%;
+    }
+    .event-blob-1,
+    .event-blob-2,
+    .event-blob-3,
+    .event-blob-4{
+        background: #009e45;
+        position: absolute;
+        width: 20vw;
+        height: 8.5vw;
+        border-radius: 7.5px;
+    }
+    .event-blob-1{
+        background: #4e4e4e;
+        margin: 5vw 15vw;
+    }
+    .event-blob-2{
+        margin: -15vw 31vw;
+    }
+    .event-blob-3{
+        margin: 5vw 45vw;
+    }
+    .event-blob-4{
+        margin: -15vw 61vw;
+    }
+    .event-name {
+        font-size: 2vw;
+        font-weight: 600;
+        letter-spacing: 1px;
+        margin-left: 1.5vw;
+    }
+    .event-duration{
+        font-size: 1.25vw;
+        font-weight: 400;
+        letter-spacing: 1px;
+        margin: -1.5vw 1.5vw;
+    }
+</style>
