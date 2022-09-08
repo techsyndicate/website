cc70238a123166d699fa2892fd738cd34f089457
Aayush Garg (He/Him) <74775129+gamer-1478@users.noreply.github.com>
2022-09-08 09:29:22 +0530

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -35,14 +35,6 @@
                 <p>Team</p>
             </li>
         </Link>
-        <li style="margin-top: -1.75vw;">
-            |
-        </li>
-        <Link to = "/intech" style="text-decoration: none; color:#eee;">
-            <li>
-                <p>inTech</p>
-            </li>
-        </Link>
     </ul>
 </div>
 <style>
