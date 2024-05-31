51f2abc12dea0fcbce1aa712852f0de486858740
Ishit Rastogi <ishitrastogi@gmail.com>
2024-06-01 00:43:15 +0530

    asdads

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -42,13 +42,6 @@
                 <p>inTech '24</p>
             </li>
         </Link>
-
-        <li style="margin-top: -1.75vw;">|</li>
-        <Link to="/sheldon" style="text-decoration: none; color:#eee;">
-            <li>
-                <p>Sheldon</p>
-            </li>
-        </Link>
     </ul>
 </div>
 <style>
